5143bf92b92321a88dc48ac3bcd7870eb17bdb13
Francinildo <francis584@gmail.com>
2020-01-13 19:51:18 -0300

    Layout for pages depends if isPrivate - Done

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,14 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from "react";
 import PropTypes from "prop-types";
 
 import { Route, Redirect } from "react-router-dom";
+
+import DefaultLayout from "../pages/_layouts/default";
+import AuthLayout from "../pages/_layouts/auth";
 
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = true;
+  const signed = false;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
@@ -18,7 +22,18 @@ export default function RouteWrapper({
     return <Redirect to="/dashboard" />;
   }
 
-  return <Route {...rest} component={Component} />;
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
