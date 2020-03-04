0d5527d1b6ca2b53f2bb2d49d508f00b50af7806
Francinildo <francis584@gmail.com>
2020-03-04 19:47:15 -0300

    create and styling Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "~/assets/logo1.svg";
+import { Container, Content, Profile } from "./styles";
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="Gobarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>Francinildo Rodrigues</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Francinildo Rodrigues"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
