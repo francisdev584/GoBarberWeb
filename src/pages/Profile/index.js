700bd720afd532dcc2a5da3c7c9782431d14dae6
Francinildo <francis584@gmail.com>
2020-03-14 14:53:09 -0300

    page profile, style - done

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Form, Input } from "@rocketseat/unform";
+import { useSelector } from "react-redux";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 export default function Profile() {
-  return <div />;
+  const Profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={Profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" type="email" />
+        <hr />
+        <Input
+          name="oldpassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmpassword"
+          type="password"
+          placeholder="confirmação de senha"
+        />
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+      <button type="button">Sair do Gobarber</button>
+    </Container>
+  );
 }
