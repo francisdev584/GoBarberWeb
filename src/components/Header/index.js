01269030179b110ba07dce72e7d6b7f53a2041b2
Francinildo <francis584@gmail.com>
2020-03-16 13:07:24 -0300

    Header - avatar photo and name from redux - done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import logo from "~/assets/logo1.svg";
@@ -7,6 +8,8 @@ import { Container, Content, Profile } from "./styles";
 import Notifications from "~/components/Notifications";
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -18,12 +21,15 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Francinildo Rodrigues</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Francinildo Rodrigues"
+              src={
+                profile.avatar.url ||
+                "https://api.adorable.io/avatars/50/abott@adorable.png"
+              }
+              alt=""
             />
           </Profile>
         </aside>
